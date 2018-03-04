7696f649af48f231c6fd426658216651c58333f1
Suman Saini <sainisuman55@gmail.com>
2018-03-03 20:36:44 -0500

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPkrKPEtaH8xop88KAWDKUBmc43q6G96I",
+    authDomain: "shoppe55555.firebaseapp.com",
+    databaseURL: "https://shoppe55555.firebaseio.com",
+    projectId: "shoppe55555",
+    storageBucket: "",
+    messagingSenderId: "902100096685"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPkrKPEtaH8xop88KAWDKUBmc43q6G96I",
+    authDomain: "shoppe55555.firebaseapp.com",
+    databaseURL: "https://shoppe55555.firebaseio.com",
+    projectId: "shoppe55555",
+    storageBucket: "",
+    messagingSenderId: "902100096685"
+  }
 };
